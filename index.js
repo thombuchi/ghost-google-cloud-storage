2919e133199eb580289364be2ef07b7206a12f23
Thomas Büchi Sagredo <tbuchi@Thomas-MacBook-Pro.local>
2015-12-26 15:31:53 -0300

    Fixed error on uploading and image link

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fs          = require('fs'),
     util        = require('util'),
     gcloud      = require('gcloud'),
     errors      = require('../../core/server/errors'),
-    utils       = require('../..//core/server/utils'),
+    utils       = require('../../core/server/utils'),
     baseStore   = require('../../core/server/storage/base'),
     options     = {},
     bucket;
@@ -18,9 +18,8 @@ function GStore(config) {
         projectId: options.projectId,
         keyFilename: options.key
     });
-
     bucket = gcs.bucket(options.bucket);
-    Promise.promisifyAll(bucket);
+
 }
 
 util.inherits(GStore, baseStore);
@@ -30,15 +29,35 @@ GStore.prototype.save = function(image) {
     if (!options) return Promise.reject('google cloud storage is not configured');
 
     var targetDir = _self.getTargetDir(),
-    googleStoragePath = options.bucket + 'storage.googleapis.com/',
+    googleStoragePath = 'https://' + options.bucket + '.storage.googleapis.com/',
     targetFilename;
 
     return this.getUniqueFileName(this, image, targetDir).then(function (filename) {
         targetFilename = filename
         var opts = {
-            destination: targetDir+ targetFilename
+            destination: targetDir + targetFilename
         };
-        return bucket.upload(image.path, opts);
+        return new Promise(function(resolve, reject) {
+            bucket.upload(image.path, opts, function(err, file) {
+                if(err) {
+                    reject(err);
+                    return;
+                }
+                resolve(file);
+                return;
+            });
+        })
+    }).then(function(file){
+        return new Promise(function(resolve, reject) {
+            file.makePublic(function(err, apiResponse) {
+                if(err) {
+                    reject(err);
+                    return; 
+                }
+                resolve();
+                return;
+            });
+        })
     }).then(function () {
         return googleStoragePath + targetDir + targetFilename;
     }).catch(function (e) {
